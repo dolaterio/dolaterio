8f228fd56efa0adeb6c56584584a468b78c456b2
Albert Callarisa <shark234@gmail.com>
2015-04-17 23:21:33 +0200

    Rethinkdb missing env error

--- api/db.go
+++ api/db.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"errors"
 	"os"
 
 	"github.com/dancannon/gorethink"
@@ -15,10 +16,17 @@ var (
 
 	// JobTable represents the jobs table
 	JobTable gorethink.Term
+
+	INVALID_DB_ADDRESS = errors.New(
+		"Invalid RethinkDB address. Please make sure $RETHINKDB_ADDRESS is properly set.")
 )
 
 // ConnectDb initializes the DB connection
 func ConnectDb() error {
+	address := os.Getenv("RETHINKDB_ADDRESS")
+	if address == "" {
+		return INVALID_DB_ADDRESS
+	}
 	session, err := gorethink.Connect(gorethink.ConnectOpts{
 		Address:  os.Getenv("RETHINKDB_ADDRESS"),
 		Database: "dolaterio",
