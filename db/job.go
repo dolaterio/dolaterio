9c92a3fa337f8c5c3d96bd4acd5e6aab74de4281
Albert Callarisa <shark234@gmail.com>
2015-07-18 23:24:14 -0700

    Logging for models

--- db/job.go
+++ db/job.go
@@ -1,5 +1,7 @@
 package db
 
+import "github.com/Sirupsen/logrus"
+
 // Job is the model struct for jobs
 type Job struct {
 	ID       string            `gorethink:"id,omitempty" json:"id"`
@@ -19,48 +21,71 @@ const (
 	StatusFinished = "finished"
 )
 
+var (
+	jobLog = logrus.WithFields(logrus.Fields{
+		"package": "db",
+		"model":   "job",
+	})
+)
+
 // GetJob returns a job from the db
 func GetJob(c *Connection, id string) (*Job, error) {
+	logFields := logrus.Fields{"id": id}
+	jobLog.WithFields(logFields).Info("Fetching job")
+
 	res, err := c.jobsTable.Get(id).Run(c.s)
 	defer res.Close()
 	if err != nil {
+		jobLog.WithFields(logFields).WithField("err", err).Error("Error fetching job")
 		return nil, err
 	}
 	if res.IsNil() {
+		jobLog.WithFields(logFields).Debug("Job not found")
 		return nil, nil
 	}
 	var job Job
 	err = res.One(&job)
 	if err != nil {
+		jobLog.WithFields(logFields).WithField("err", err).Error("Error loading job")
 		return nil, err
 	}
 	job.Worker, err = GetWorker(c, job.WorkerID)
 	if err != nil {
+		jobLog.WithFields(logFields).WithField("err", err).Error("Error getting job's worker")
 		return nil, err
 	}
+	jobLog.WithFields(logFields).WithField("job", job).Debug("Retrieved job")
 	return &job, nil
 }
 
 // Store inserts the job into the db
 func (job *Job) Store(c *Connection) error {
+	jobLog.Info("Storing job")
+	jobLog.WithField("job", job).Debug("Job object")
+
 	job.Status = StatusQueued
 
 	res, err := c.jobsTable.Insert(job).RunWrite(c.s)
 	if err != nil {
+		jobLog.WithField("err", err).Error("Error storing the job")
 		return err
 	}
 	if len(res.GeneratedKeys) < 1 {
+		jobLog.Error("Job not saved")
 		return nil
 	}
 	job.ID = res.GeneratedKeys[0]
+	jobLog.WithField("job", job).Debug("Job saved")
 
 	return nil
 }
 
 // Update updates the job into the db.
 func (job *Job) Update(c *Connection) error {
+	jobLog.WithField("id", job.ID).Info("Updating job")
 	_, err := c.jobsTable.Update(job).RunWrite(c.s)
 	if err != nil {
+		jobLog.WithField("err", err).Error("Error updating the job")
 		return err
 	}
 	return nil
--- db/worker.go
+++ db/worker.go
@@ -1,6 +1,10 @@
 package db
 
-import "time"
+import (
+	"time"
+
+	"github.com/Sirupsen/logrus"
+)
 
 // Worker is the model struct for workers
 type Worker struct {
@@ -11,19 +15,31 @@ type Worker struct {
 	Timeout     time.Duration     `gorethink:"timeout,omitempty" json:"timeout"`
 }
 
+var (
+	workerLog = logrus.WithFields(logrus.Fields{
+		"package": "db",
+		"model":   "worker",
+	})
+)
+
 // GetWorker returns a worker from the db
 func GetWorker(c *Connection, id string) (*Worker, error) {
+	logFields := logrus.Fields{"id": id}
+	workerLog.WithFields(logFields).Info("Fetching worker")
 	res, err := c.workersTable.Get(id).Run(c.s)
 	defer res.Close()
 	if err != nil {
+		workerLog.WithFields(logFields).WithField("err", err).Error("Error fetching worker")
 		return nil, err
 	}
 	if res.IsNil() {
+		workerLog.WithFields(logFields).Debug("Worker not found")
 		return nil, nil
 	}
 	var worker Worker
 	err = res.One(&worker)
 	if err != nil {
+		workerLog.WithFields(logFields).WithField("err", err).Error("Error loading worker")
 		return nil, err
 	}
 	return &worker, nil
@@ -31,22 +47,30 @@ func GetWorker(c *Connection, id string) (*Worker, error) {
 
 // Store inserts the worker into the db
 func (worker *Worker) Store(c *Connection) error {
+	workerLog.Info("Storing worker")
+	workerLog.WithField("worker", worker).Debug("Worker object")
+
 	res, err := c.workersTable.Insert(worker).RunWrite(c.s)
 	if err != nil {
+		workerLog.WithField("err", err).Error("Error storing the worker")
 		return err
 	}
 	if len(res.GeneratedKeys) < 1 {
+		workerLog.Error("Worker not saved")
 		return nil
 	}
 	worker.ID = res.GeneratedKeys[0]
+	workerLog.WithField("worker", worker).Debug("Worker saved")
 
 	return nil
 }
 
 // Update updates the worker into the db.
 func (worker *Worker) Update(c *Connection) error {
+	workerLog.WithField("id", worker.ID).Info("Updating worker")
 	_, err := c.workersTable.Update(worker).RunWrite(c.s)
 	if err != nil {
+		workerLog.WithField("err", err).Error("Error updating the worker")
 		return err
 	}
 	return nil
